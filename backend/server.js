8289725e88e0c600debf87a67d2e9d7daa463381
Juliusz <juliusz.kusiak@gmail.com>
2021-06-04 21:33:53 +0200

    Solve loading of environmental variables

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,25 @@
 import connectDB from './config/db.js';
 import dotenv  from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoute.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const PORT = process.env.PORT || 5000;
-const MODE = process.env.NODE_ENV;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+dotenv.config({path:`${__dirname} /../.env`});
 connectDB();
 
-dotenv.config();
+const PORT = process.env.PORT;
+const MODE = process.env.NODE_ENV;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use('/api/users', userRoutes);
 
-app.listen(PORT, console.log(`App is running in ${MODE} mode on port ${PORT}`))
+app.listen(PORT, () => {
+      console.log(`App is running in ${MODE} mode on port ${PORT}`)
+});
